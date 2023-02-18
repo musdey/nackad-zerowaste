0333435e372f6b4efe93c5934228724e7994d534
Daniel V Sam <Sam.DanielV@in.bosch.com>
2023-02-18 15:21:49 +0530

    update component to show existing images

--- server/frontend/src/pages/Images.tsx
+++ server/frontend/src/pages/Images.tsx
@@ -7,24 +7,56 @@ import {
   IonGrid,
   IonCard,
   IonButton,
+  IonImg,
+  IonSpinner,
 } from "@ionic/react";
 import { useAuth } from "../lib/use-auth";
 import { Redirect, useParams } from "react-router";
 import { Header } from "../components/Header";
 import api from "../lib/api";
 
-const Images: React.FC = (props) => {
+type ImageCardProps = {
+  deliveryId: string;
+  imageId: string;
+};
+
+const ImageCard: React.FC<ImageCardProps> = (props: ImageCardProps) => {
+  const [source, setSource] = useState("");
+  useEffect(() => {
+    const fn = async () => {
+      const fetched = await api.getImage(props.deliveryId, props.imageId);
+      const newUrl = URL.createObjectURL(fetched);
+      setSource(newUrl);
+    };
+    fn();
+  }, []);
+  return (
+    <IonCard>
+      {source ? (
+        <div>
+          <IonImg alt="uploaded_image" src={source} />
+          <IonButton fill="clear">Delete</IonButton>
+        </div>
+      ) : (
+        <IonSpinner name="crescent"></IonSpinner>
+      )}
+    </IonCard>
+  );
+};
+
+const Images: React.FC = () => {
   const { loggedIn } = useAuth();
   const params = useParams<{ deliveryId: string }>();
   const [images, setImages] = useState<Array<string> | undefined>();
-  const [source, setSource] = useState("");
+  const [uploading, setUploading] = useState<boolean>(false);
+
+  const getDelivery = async () => {
+    const result = await api.getDelivery(params.deliveryId);
+    setImages(result.images || undefined);
+  };
 
   useEffect(() => {
-    const fn = async () => {
-      const result = await api.getDelivery(params.deliveryId);
-      setImages(result.images || undefined);
-    };
-    fn();
+    getDelivery();
   }, []);
 
   if (!loggedIn) {
@@ -34,10 +66,11 @@ const Images: React.FC = (props) => {
   const setImage = (_event: any) => {
     if (_event.target.files && _event.target.files.length !== 0) {
       let file = _event.target.files![0];
-      console.log(file);
-      const newUrl = URL.createObjectURL(file);
-      setSource(newUrl);
-      api.sendImage(params.deliveryId, file);
+      setUploading(true);
+      api.sendImage(params.deliveryId, file).then(() => {
+        setUploading(false);
+        getDelivery();
+      });
     }
   };
   const openFileDialog = () => {
@@ -53,6 +86,7 @@ const Images: React.FC = (props) => {
             display: "flex",
             width: "100%",
             justifyContent: "center",
+            alignItems: "center",
           }}>
           <input
             style={{ display: "none" }}
@@ -65,11 +99,12 @@ const Images: React.FC = (props) => {
           <label onClick={openFileDialog} htmlFor="icon-button-file">
             <IonButton>Upload</IonButton>
           </label>
+          {uploading && <IonSpinner name="crescent"></IonSpinner>}
         </div>
-        <IonCard>
-          <img alt="uploaded_image" src={source} />
-          <IonButton fill="clear">Delete</IonButton>
-        </IonCard>
+        {images &&
+          images.map((image) => (
+            <ImageCard deliveryId={params.deliveryId} imageId={image} />
+          ))}
       </IonContent>
       <IonFooter>
         <IonGrid className="ion-no-margin ion-no-padding"></IonGrid>
