bb58a067e6a7f2599926a4cd0716fbe34305f032
Mustafa Cicek <swave94@gmail.com>
2023-03-29 20:35:47 +0200

    Update, sort & group deliver format in backend.

--- server/backend/src/controller/delivery.controller.ts
+++ server/backend/src/controller/delivery.controller.ts
@@ -1,5 +1,4 @@
 import DeliveryModel from '../models/Delivery'
-import { IShop } from '../models/Shop'
 import User, { IUser } from '../models/User'
 
 const getAll = async (shop: string) => {
@@ -14,7 +13,7 @@ const getAllWithStatus = async (status: 'OPEN' | 'INDELIVERY' | 'DELIVERED' | 'C
 
 const getCurrent = async (shop: string) => {
   const todayMorning = new Date(Date.now())
-  todayMorning.setHours(0)
+  todayMorning.setHours(3)
   todayMorning.setMinutes(0)
   const delivery = await DeliveryModel.find({
     shop,
@@ -22,7 +21,93 @@ const getCurrent = async (shop: string) => {
       $gte: todayMorning
     }
   }).populate('deliverySlot user')
-  return delivery
+
+  const sorted = delivery.sort((a: any, b: any) => {
+    // sort by deliveryDay
+    if (a.deliverySlot.deliveryDay && b.deliverySlot.deliveryDay) {
+      if (a.deliverySlot.deliveryDay < b.deliverySlot.deliveryDay) {
+        return -1
+      } else if (a.deliverySlot.deliveryDay > b.deliverySlot.deliveryDay) {
+        return 1
+      } else {
+        // sort by vehicle/deliverySlot
+        if (a.deliverySlot._id > b.deliverySlot._id) {
+          return -1
+        } else if (a.deliverySlot._id < b.deliverySlot._id) {
+          return 1
+        } else {
+          // sort by postalCode
+          if (a.address.zip <= b.address.zip) {
+            return -1
+          } else {
+            return 1
+          }
+        }
+      }
+    } else {
+      return 0
+    }
+  })
+
+  const grouped: any[] = []
+
+  // group by sorted.deliveryDay
+  sorted.forEach((delivery: any) => {
+    const deliveryDay = delivery.deliveryDay
+    const index = grouped.findIndex((group: any) => group.deliveryDay.toString() == deliveryDay.toString())
+
+    if (index === -1) {
+      grouped.push({
+        deliveryDay,
+        deliveries: [delivery]
+      })
+    } else {
+      grouped[index].deliveries.push(delivery)
+    }
+  })
+
+  const groupedDeliveries: any[] = []
+
+  grouped.forEach((delivery) => {
+    const deliveryDay = delivery.deliveryDay
+
+    // Find or create the delivery day group
+    let deliveryDayGroup = groupedDeliveries.find((group) => group.deliveryDay === deliveryDay)
+    if (!deliveryDayGroup) {
+      deliveryDayGroup = { deliveryDay, vehicles: [] }
+      groupedDeliveries.push(deliveryDayGroup)
+    }
+
+    // Group the deliveries by vehicle
+    delivery.deliveries.forEach((deliveryItem: any) => {
+      const vehicleId = deliveryItem.deliverySlot.vehicleId
+
+      // Find or create the vehicle group
+      let vehicleGroup = deliveryDayGroup.vehicles.find((group: any) => group.vehicleId === vehicleId)
+      if (!vehicleGroup) {
+        vehicleGroup = { vehicleId, deliveries: [] }
+        deliveryDayGroup.vehicles.push(vehicleGroup)
+      }
+
+      // Add the delivery to the vehicle group
+      vehicleGroup.deliveries.push(deliveryItem)
+    })
+  })
+
+  // sort the nested vehicles by vehicleId
+  groupedDeliveries.forEach((deliveryDayGroup) => {
+    deliveryDayGroup.vehicles.sort((a: any, b: any) => {
+      if (a.vehicleId < b.vehicleId) {
+        return -1
+      } else if (a.vehicleId > b.vehicleId) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  })
+
+  return groupedDeliveries
 }
 
 const getTodays = async (shop: string) => {
